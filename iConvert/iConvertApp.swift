2382c9ed8ffa590c3b65f886ce303a18d14eb983
bektemur-07 <104037405+bektemur-07@users.noreply.github.com>
2022-12-26 11:33:32 +0500

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Bektemur on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
